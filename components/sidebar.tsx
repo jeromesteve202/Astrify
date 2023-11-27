4840170692affcf52e3bfa8f79b117a9bc65b761
Steve Sahayadarlin <jeromesteve202@gmail.com>
2023-11-27 00:25:34 -0500

    adding code generation and conversation page

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -58,7 +58,7 @@ const Sidebar = () => {
     const pathname = usePathname();
 
     return ( 
-        <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+        <div className="space-y-4 py-4 flex flex-col h-full bg-[#f5f6f8] text-black">
             <div className="px-3 py-2 flex-1">
                 <Link href="/dashboard" className="flex items-center pl-3 mb-14">
                     <div className="relative w-8 h-8 mr-4">
@@ -74,7 +74,7 @@ const Sidebar = () => {
                 </Link>
                 <div className="space-y-1">
                     {routes.map((route) => (
-                        <Link href={route.href} key={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}>
+                        <Link href={route.href} key={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-black hover:bg-black/10 rounded-lg transition", pathname === route.href ? "text-black bg-black/10 font-bold" : "text-zinc-500")}>
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-5 w-5 mr-3", route.color)}/>
                                 {route.label}
